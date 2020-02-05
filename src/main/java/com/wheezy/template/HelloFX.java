d5ab16f264740e3a8f4a16f52a0bb2070efc257e
Skylar Scaling <skylar.scaling@gmail.com>
2020-02-04 16:48:05 -0800

    Added exit on close and formatted source.

--- src/main/java/com/wheezy/template/HelloFX.java
+++ src/main/java/com/wheezy/template/HelloFX.java
@@ -1,30 +1,48 @@
 package com.wheezy.template;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-public class HelloFX extends Application {
+public class HelloFX extends Application
+{
 
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			// Read file fxml and draw interface.
-			Parent root = FXMLLoader.load(getClass().getResource("HelloFX.fxml"));
+  @Override
+  public void start(Stage primaryStage)
+  {
+    try
+    {
+      // Read file fxml and draw interface.
+      Parent root = FXMLLoader.load(getClass().getResource("HelloFX.fxml"));
 
-			primaryStage.setTitle("Hell World FX!");
-			primaryStage.setScene(new Scene(root));
-			primaryStage.show();
+      primaryStage.setTitle("Hell World FX!");
+      primaryStage.setScene(new Scene(root));
+      primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>()
+      {
+        @Override
+        public void handle(WindowEvent t)
+        {
+          Platform.exit();
+          System.exit(0);
+        }
+      });
+      primaryStage.show();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
 
 }
